e10add9f5f7bd418b6bc27dc2b2b162469df7439
manon-coulombe <manon.coulombe@student.adatechschool.fr>
2025-12-15 17:47:16 +0100

    add tailwind and header style

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,19 +4,23 @@ import {Challenge} from "@/app/types/challenge";
 export default function Home() {
     const challenges = challengesJson.challenges as unknown as Challenge[];
   return (
-    <body>
-      <header>Challenges</header>
-      <main>
+    <div>
+        <header className='flex  flex-col justify-center items-center h-48 bg-rose-200'>
+            <h1 className='text-2xl font-bold'>Orientation</h1>
+            <p>Clarifier et valider ton projet, identifier tes compétences, rechercher une formation ou une
+                alternance.</p>
+        </header>
+        <main>
         <ul>
             {challenges.map((
                 c =>
-                    <li key={c.id}>
+                    <li className='text-base' key={c.id}>
                         {c.title}
                     </li>
                 )
             )}
         </ul>
       </main>
-    </body>
+    </div>
   );
 }
