4c7e74036b59b60424cb5055a1d8405d82a4d7d7
manon-coulombe <manon.coulombe@student.adatechschool.fr>
2025-12-17 11:51:22 +0100

    add back button

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { use } from "react";
 import challengesJson from "../data/challenges.json";
 import {Challenge} from "@/app/types/challenge";
 import Duration from "@/app/components/duration";
+import BackButton from "@/app/components/backButton";
 
 export default function ChallengePage({params}: {params: Promise<{slug: string}>}) {
     const { slug } = use(params);
@@ -16,6 +17,7 @@ export default function ChallengePage({params}: {params: Promise<{slug: string}>
 
     return (
         <main className="w-screen h-screen p-20 bg-indigo-600">
+            <BackButton />
             <div className="max-w-sm mx-auto bg-neutral-50 p-14 border rounded-3xl">
                 <div className="flex flex-col w-full items-center mb-8">
                     <h1 className="text-3xl font-bold text-center mb-4">{challenge.title}</h1>
